4ddfa9a5d3df403ce0d6c76f99dd2d2003afec35
Ermal Kaleci <ermalkaleci@gmail.com>
2023-08-31 10:53:32 +0200

    refactor

--- bencher/src/build_wasm/mod.rs
+++ bencher/src/build_wasm/mod.rs
@@ -1,14 +1,103 @@
 use rand::{distributions::Alphanumeric, thread_rng, Rng};
+use std::{
+	fs,
+	path::{Path, PathBuf},
+	process::Command,
+};
 
-pub mod prerequisites;
-pub mod wasm_project;
+mod prerequisites;
+mod wasm_project;
 
-/// Environment variable to disable color output of the wasm build.
-const WASM_BUILD_NO_COLOR: &str = "WASM_BUILD_NO_COLOR";
+/// Environment variable that tells us to skip building the wasm binary.
+const SKIP_BUILD_ENV: &str = "SKIP_WASM_BUILD";
 
-/// Returns `true` when color output is enabled.
-pub fn color_output_enabled() -> bool {
-	std::env::var(WASM_BUILD_NO_COLOR).is_err()
+/// Environment variable that tells us whether we should avoid network requests
+const OFFLINE: &str = "CARGO_NET_OFFLINE";
+
+/// Environment variable to force a certain build type when building the wasm binary.
+/// Expects "debug", "release" or "production" as value.
+///
+/// When unset the WASM binary uses the same build type as the main cargo build with
+/// the exception of a debug build: In this case the wasm build defaults to `release` in
+/// order to avoid a slowdown when not explicitly requested.
+const WASM_BUILD_TYPE_ENV: &str = "WASM_BUILD_TYPE";
+
+/// Environment variable to extend the `RUSTFLAGS` variable given to the wasm build.
+const WASM_BUILD_RUSTFLAGS_ENV: &str = "WASM_BUILD_RUSTFLAGS";
+
+/// Environment variable to set the target directory to copy the final wasm binary.
+///
+/// The directory needs to be an absolute path.
+const WASM_TARGET_DIRECTORY: &str = "WASM_TARGET_DIRECTORY";
+
+/// Environment variable that hints the workspace we are building.
+const WASM_BUILD_WORKSPACE_HINT: &str = "WASM_BUILD_WORKSPACE_HINT";
+
+/// Write to the given `file` if the `content` is different.
+fn write_file_if_changed(file: impl AsRef<Path>, content: impl AsRef<str>) {
+	if fs::read_to_string(file.as_ref()).ok().as_deref() != Some(content.as_ref()) {
+		fs::write(file.as_ref(), content.as_ref())
+			.unwrap_or_else(|_| panic!("Writing `{}` can not fail!", file.as_ref().display()));
+	}
+}
+
+/// Copy `src` to `dst` if the `dst` does not exist or is different.
+fn copy_file_if_changed(src: PathBuf, dst: PathBuf) {
+	let src_file = fs::read_to_string(&src).ok();
+	let dst_file = fs::read_to_string(&dst).ok();
+
+	if src_file != dst_file {
+		fs::copy(&src, &dst).unwrap_or_else(|_| {
+			panic!(
+				"Copying `{}` to `{}` can not fail; qed",
+				src.display(),
+				dst.display()
+			)
+		});
+	}
+}
+
+/// Wraps a specific command which represents a cargo invocation.
+#[derive(Debug)]
+pub struct CargoCommand {
+	program: String,
+}
+
+impl CargoCommand {
+	fn new(program: &str) -> Self {
+		CargoCommand {
+			program: program.into(),
+		}
+	}
+
+	fn command(&self) -> Command {
+		Command::new(&self.program)
+	}
+}
+
+/// Wraps a [`CargoCommand`] and the version of `rustc` the cargo command uses.
+pub struct CargoCommandVersioned {
+	command: CargoCommand,
+	version: String,
+}
+
+impl CargoCommandVersioned {
+	pub fn new(command: CargoCommand, version: String) -> Self {
+		Self { command, version }
+	}
+
+	/// Returns the `rustc` version.
+	pub fn rustc_version(&self) -> &str {
+		&self.version
+	}
+}
+
+impl std::ops::Deref for CargoCommandVersioned {
+	type Target = CargoCommand;
+
+	fn deref(&self) -> &CargoCommand {
+		&self.command
+	}
 }
 
 pub fn build() -> std::io::Result<Vec<u8>> {
@@ -21,8 +110,9 @@ pub fn build() -> std::io::Result<Vec<u8>> {
 		.map(char::from)
 		.collect::<String>();
 
+	let profile = wasm_project::Profile::detect(&std::env::current_dir()?).name();
 	let mut out_dir = std::path::PathBuf::from(manifest_dir);
-	out_dir.push(format!("target/release/build/{}-{}/out", pkg_name, random));
+	out_dir.push(format!("target/{profile}/build/{pkg_name}-{random}/out"));
 
 	std::env::set_var("OUT_DIR", out_dir.display().to_string());
 
@@ -44,6 +134,7 @@ pub fn build() -> std::io::Result<Vec<u8>> {
 		cargo_cmd,
 		vec!["wasm-bench".to_string()],
 		None,
+		false,
 	);
 
 	let (wasm_binary, _wasm_binary_bloaty) = if let Some(wasm_binary) = wasm_binary {
--- bencher/src/build_wasm/prerequisites.rs
+++ bencher/src/build_wasm/prerequisites.rs
@@ -1,182 +1,6 @@
-// This file is part of Substrate.
-
-// Copyright (C) 2019-2021 Parity Technologies (UK) Ltd.
-// SPDX-License-Identifier: Apache-2.0
-
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// 	http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
+use super::{CargoCommand, CargoCommandVersioned};
 use crate::colorize::{color_output_enabled, red_bold, yellow_bold};
-use std::{
-	env, fs,
-	io::BufRead,
-	path::{Path, PathBuf},
-	process::Command,
-};
-
-/// Environment variable to set the toolchain used to compile the wasm binary.
-pub const WASM_BUILD_TOOLCHAIN: &str = "WASM_BUILD_TOOLCHAIN";
-
-/// Write to the given `file` if the `content` is different.
-pub fn write_file_if_changed(file: impl AsRef<Path>, content: impl AsRef<str>) {
-	if fs::read_to_string(file.as_ref()).ok().as_deref() != Some(content.as_ref()) {
-		fs::write(file.as_ref(), content.as_ref())
-			.unwrap_or_else(|_| panic!("Writing `{}` can not fail!", file.as_ref().display()));
-	}
-}
-
-/// Copy `src` to `dst` if the `dst` does not exist or is different.
-pub fn copy_file_if_changed(src: PathBuf, dst: PathBuf) {
-	let src_file = fs::read_to_string(&src).ok();
-	let dst_file = fs::read_to_string(&dst).ok();
-
-	if src_file != dst_file {
-		fs::copy(&src, &dst).unwrap_or_else(|_| {
-			panic!(
-				"Copying `{}` to `{}` can not fail; qed",
-				src.display(),
-				dst.display()
-			)
-		});
-	}
-}
-
-/// Get a cargo command that compiles with nightly
-fn get_nightly_cargo() -> CargoCommand {
-	let env_cargo =
-		CargoCommand::new(&env::var("CARGO").expect("`CARGO` env variable is always set by cargo"));
-	let default_cargo = CargoCommand::new("cargo");
-	let rustup_run_nightly = CargoCommand::new_with_args("rustup", &["run", "nightly", "cargo"]);
-	let wasm_toolchain = env::var(WASM_BUILD_TOOLCHAIN).ok();
-
-	// First check if the user requested a specific toolchain
-	if let Some(cmd) = wasm_toolchain.and_then(|t| get_rustup_nightly(Some(t))) {
-		cmd
-	} else if env_cargo.is_nightly() {
-		env_cargo
-	} else if default_cargo.is_nightly() {
-		default_cargo
-	} else if rustup_run_nightly.is_nightly() {
-		rustup_run_nightly
-	} else {
-		// If no command before provided us with a nightly compiler, we try to search
-		// one with rustup. If that fails as well, we return the default cargo and let
-		// the prequisities check fail.
-		get_rustup_nightly(None).unwrap_or(default_cargo)
-	}
-}
-
-/// Get a nightly from rustup. If `selected` is `Some(_)`, a `CargoCommand`
-/// using the given nightly is returned.
-fn get_rustup_nightly(selected: Option<String>) -> Option<CargoCommand> {
-	let host = format!(
-		"-{}",
-		env::var("HOST").expect("`HOST` is always set by cargo")
-	);
-
-	let version = match selected {
-		Some(selected) => selected,
-		None => {
-			let output = Command::new("rustup").args(["toolchain", "list"]).output().ok()?.stdout;
-			let lines = output.as_slice().lines();
-
-			let mut latest_nightly = None;
-			for line in lines.filter_map(|l| l.ok()) {
-				if line.starts_with("nightly-") && line.ends_with(&host) {
-					// Rustup prints them sorted
-					latest_nightly = Some(line.clone());
-				}
-			}
-
-			latest_nightly?.trim_end_matches(&host).into()
-		},
-	};
-
-	Some(CargoCommand::new_with_args(
-		"rustup",
-		&["run", &version, "cargo"],
-	))
-}
-
-/// Wraps a specific command which represents a cargo invocation.
-#[derive(Debug)]
-pub struct CargoCommand {
-	program: String,
-	args: Vec<String>,
-}
-
-impl CargoCommand {
-	fn new(program: &str) -> Self {
-		CargoCommand {
-			program: program.into(),
-			args: Vec::new(),
-		}
-	}
-
-	fn new_with_args(program: &str, args: &[&str]) -> Self {
-		CargoCommand {
-			program: program.into(),
-			args: args.iter().map(ToString::to_string).collect(),
-		}
-	}
-
-	pub fn command(&self) -> Command {
-		let mut cmd = Command::new(&self.program);
-		cmd.args(&self.args);
-		cmd
-	}
-
-	/// Check if the supplied cargo command is a nightly version
-	fn is_nightly(&self) -> bool {
-		// `RUSTC_BOOTSTRAP` tells a stable compiler to behave like a nightly. So, when
-		// this env variable is set, we can assume that whatever rust compiler we have,
-		// it is a nightly compiler. For "more" information, see:
-		// https://github.com/rust-lang/rust/blob/fa0f7d0080d8e7e9eb20aa9cbf8013f96c81287f/src/libsyntax/feature_gate/check.rs#L891
-		env::var("RUSTC_BOOTSTRAP").is_ok()
-			|| self
-				.command()
-				.arg("--version")
-				.output()
-				.map_err(|_| ())
-				.and_then(|o| String::from_utf8(o.stdout).map_err(|_| ()))
-				.unwrap_or_default()
-				.contains("-nightly")
-	}
-}
-
-/// Wraps a [`CargoCommand`] and the version of `rustc` the cargo command uses.
-pub struct CargoCommandVersioned {
-	command: CargoCommand,
-	version: String,
-}
-
-impl CargoCommandVersioned {
-	fn new(command: CargoCommand, version: String) -> Self {
-		Self { command, version }
-	}
-
-	/// Returns the `rustc` version.
-	pub fn rustc_version(&self) -> &str {
-		&self.version
-	}
-}
-
-impl std::ops::Deref for CargoCommandVersioned {
-	type Target = CargoCommand;
-
-	fn deref(&self) -> &CargoCommand {
-		&self.command
-	}
-}
+use std::{fs, path::Path};
 
 use tempfile::tempdir;
 
@@ -184,11 +8,7 @@ use tempfile::tempdir;
 ///
 /// Returns the versioned cargo command on success.
 pub fn check() -> Result<CargoCommandVersioned, String> {
-	let cargo_command = get_nightly_cargo();
-
-	if !cargo_command.is_nightly() {
-		return Err(red_bold("Rust nightly not installed, please install it!"));
-	}
+	let cargo_command = CargoCommand::new("cargo");
 
 	check_wasm_toolchain_installed(cargo_command)
 }
@@ -201,7 +21,7 @@ fn create_check_toolchain_project(project_dir: &Path) {
 	let build_rs_file = project_dir.join("build.rs");
 	let manifest_path = project_dir.join("Cargo.toml");
 
-	write_file_if_changed(
+	super::write_file_if_changed(
 		manifest_path,
 		r#"
 			[package]
@@ -217,7 +37,7 @@ fn create_check_toolchain_project(project_dir: &Path) {
 			[workspace]
 		"#,
 	);
-	write_file_if_changed(lib_rs_file, "pub fn test() {}");
+	super::write_file_if_changed(lib_rs_file, "pub fn test() {}");
 
 	// We want to know the rustc version of the rustc that is being used by our
 	// cargo command. The cargo command is determined by some *very* complex
@@ -228,7 +48,7 @@ fn create_check_toolchain_project(project_dir: &Path) {
 	// process. This `build.rs` is exactly doing this. It gets the rustc version by
 	// calling `rustc --version` and exposing it in the `RUSTC_VERSION` environment
 	// variable.
-	write_file_if_changed(
+	super::write_file_if_changed(
 		build_rs_file,
 		r#"
 			fn main() {
@@ -249,7 +69,7 @@ fn create_check_toolchain_project(project_dir: &Path) {
 	);
 	// Just prints the `RURSTC_VERSION` environment variable that is being created
 	// by the `build.rs` script.
-	write_file_if_changed(
+	super::write_file_if_changed(
 		main_rs_file,
 		r#"
 			fn main() {
